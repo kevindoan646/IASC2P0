b5a062d8152ae0f8b1ee7fd431a5d4b2070fd874
kevindoan646 <31972309+kevindoan646@users.noreply.github.com>
2017-12-06 19:35:32 -0500

    Add files via upload
    https://wireframe.cc/SplK9c
    https://wireframe.cc/TECCjz
    https://wireframe.cc/pSzDbk

---
+++ navbar.js
@@ -0,0 +1,24 @@
+document.write('\
+\
+    <nav class="navbar navbar-inverse navbar-static-top">\
+	<div class="container">\
+		<div class="navbar-header">\
+			<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#collapse-bar" aria-expanded="false">\
+				<span class="sr-only">Toggle navigation</span>\
+				<span class="icon-bar"></span>\
+				<span class="icon-bar"></span>\
+				<span class="icon-bar"></span>\
+			</button>\
+			<a class="navbar-brand" href="#">My Student Portfolio</a>\
+		</div>\
+		<div class="collapse navbar-collapse" id="collapse-bar">\
+			<ul class="nav navbar-nav navbar-right">\
+				<li><a href="kevin_site.html">About</a></li>\
+				<li><a href="kevin_site_p2.html">Education</a></li>\
+				<li><a href="kevin_site_p3.html">Facts</a></li>\
+			</ul>\
+		</div>\
+	</div>\
+</nav>\
+\
+');
